03ec228cb596a2c8a8347e0508d03d4e8397a912
Jackson <35212897+actuarialcat@users.noreply.github.com>
2020-10-25 17:56:18 +0800

    start to use js

---
+++ answer_check_v1.js
@@ -0,0 +1,14 @@
+
+
+
+
+function check_ans() {
+    var x = document.getElementById("answer_input").value;
+  
+    console.log(x)
+  
+    document.getElementById("output").innerHTML = x;
+}
+
+
+
