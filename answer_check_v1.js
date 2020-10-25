9661788994aa9c05e23c500b1dc8c13fbc99b734
Jackson <35212897+actuarialcat@users.noreply.github.com>
2020-10-26 00:19:14 +0800

    update

--- answer_check_v1.js
+++ answer_check_v1.js
@@ -1,6 +1,5 @@
 
 
-
 /////////////////////////////////////////////////
 // HTML Functions
 
@@ -15,28 +14,42 @@ function check_ans() {
     document.getElementById("output_inp").innerHTML = output[0];
     document.getElementById("output_correct").innerHTML = output[1];
     document.getElementById("final_result").innerHTML = output[2];
+    document.getElementById("step_result").innerHTML = output[3];
 }
 
 
 
 function html_repeat_input(inp_str) {
 
-    var input = inp_str.split("=");
+    var input = inp_str.split("\n");
 
-    if (input[0] == "") {
-        // remove first empty element
+    input.trim_head_tail();
+    input.trim_spaces();
+    input.trim_equals();
+    input.remove_empty_element();
+
+    while (input[0] == "") {
+        // remove leading empty element
         input.shift();
+    }
+    while (input[input.length-1] == "") {
+        // remove leading empty element
+        input.pop();
     }
 
     const ANS = 5
 
-    var result_1 = bool_to_output_sting(milestone_1(input, ANS));
-    
-    var output_1 = display_each_line(input, "= ");
-    var output_2 = display_each_line(result_1);
-    var output_3 = "The final answer is " + bool_to_output_sting(check_final_answer(input, ANS));
+    var result_1 = milestone_1(input, ANS);
 
-    return [output_1, output_2, output_3];
+    var output_1 = display_each_line(input, "= ");
+    var output_2 = display_each_line(bool_to_output_sting(result_1[0]));
+    var output_3 = "The final answer is " + bool_to_output_sting(check_final_answer(input, ANS));
+    var output_4 = bool_to_output_sting(result_1[1], 
+        "All steps are correct",
+        "Some steps are wrong"
+        );
+
+    return [output_1, output_2, output_3, output_4];
 }
 
 
@@ -106,6 +119,11 @@ function check_final_answer(input, ANS) {
 
 function milestone_1(input, ans) {
     var result = [];
+    var all_correct = true;
+
+    if (input.length) {
+        all_correct = false;
+    }
 
     for (var i = 0; i < input.length; i++) {
         if (string_calculate(input[i]) == ans) {
@@ -113,10 +131,11 @@ function milestone_1(input, ans) {
         }
         else {
             result[i] = false;
+            all_correct = false;
         }
     }
     
-    return result;
+    return [result, all_correct];
 }
 
 
@@ -136,6 +155,47 @@ function string_calculate(obj) {
     
 }
 
+
+/////////////////////////////////////////////////
+// Utility Functions
+
+String.prototype.trim_head_tail = function() {
+    return String(this).replace(/^\s+|\s+$/g, '');
+};
+
+Array.prototype.trim_head_tail = function() {
+    for (var i = 0; i < this.length; i++){
+        this[i] = this[i].trim_head_tail();
+    }
+}
+
+String.prototype.trim_spaces = function() {
+    return String(this).replace(/ /g, '');
+};
+
+Array.prototype.trim_spaces = function() {
+    for (var i = 0; i < this.length; i++){
+        this[i] = this[i].trim_spaces();
+    }
+}
+
+String.prototype.trim_equals = function() {
+    return String(this).replace(/=/g, '');
+};
+
+Array.prototype.trim_equals = function() {
+    for (var i = 0; i < this.length; i++){
+        this[i] = this[i].trim_equals();
+    }
+}
+
+Array.prototype.remove_empty_element = function() {
+    for (var i = 0; i < this.length; i++){
+        if(this[i] == ""){
+            this.splice(i,1);
+        }
+    }
+}
 
 
 
